2b560b29d0a1a069f03131ac7b757a66e5499299
K4T4L1N <85583988+K4T4L1N@users.noreply.github.com>
2021-06-19 22:11:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ app = Flask(__name__)
 
 @app. route("/")
 def hello():
-try:
-visits = redis.incr("counter")
-except RedisError:
-visits = "<i>cannot connect to Redis, counter disabled</i>”
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>”
 
-html = ”<h3>Hello {name}!</h3>” \
-"<b>Hostname:</b> {hostname}<br/>" \
-"<b>Visits:</b> {visits}”
-return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}”
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main___":
 app.run(host='0.0.0.0', port=80)
